4274c0dd42d2d013fa0bab250555d431cec3ae95
Boris-al <lyubavin.boris@gmail.com>
2020-09-17 18:22:37 +0300

    homework 2

--- 1_date_and_time.py
+++ 1_date_and_time.py
@@ -1,3 +1,5 @@
+from datetime import date, timedelta, datetime
+from calendar import monthrange
 """
 Домашнее задание №2
 
@@ -13,7 +15,19 @@ def print_days():
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    datetime_now = date.today()
+    datetime_yesturday = datetime_now - timedelta(1,)
+    month = date.today().strftime("%m")
+    year = date.today().strftime("%Y")
+    num_of_days = monthrange(int(year), int(month))
+    datetime_month_ago = datetime_now - timedelta(num_of_days[1],)
+
+
+    print(f"Вчера {datetime_yesturday}")
+    print(f"Сегодня {datetime_now}")
+    print(f'Месяц назад {datetime_month_ago}')
+
+  
 
 
 def str_2_datetime(date_string):
@@ -21,7 +35,9 @@ def str_2_datetime(date_string):
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+   
+    date_result = datetime.strptime(date_string, '%m/%d/%y %H:%M:%S.%f')
+    return date_result
 
 if __name__ == "__main__":
     print_days()
--- 3_dict_to_csv.py
+++ 3_dict_to_csv.py
@@ -1,3 +1,4 @@
+import csv 
 """
 Домашнее задание №2
 
@@ -13,7 +14,20 @@ def main():
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    name_age_job = [
+        {'name': 'Bjorn', 'age': 19},
+        {'name': 'John', 'age': 99, 'job': 'athlete'},
+        {'name': 'Kshishtof', 'age': 79, 'job': 'director'},
+        {'name': 'Kate', 'age': 33, 'job': 'surfer'},
+        {'name': 'Alisa', 'age': 22, 'job': 'developer'}  
+    ]
+    with open('example4.csv', 'w', newline='', restval='raise') as csvfile:
+        fieldnames = ['name', 'age', 'job']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(name_age_job)
+        
+    print("Writing complete")
 
 if __name__ == "__main__":
     main()
