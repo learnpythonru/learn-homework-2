51ae50402a7aa66bea4afc8254269008367d14d0
saxetr <test@testmail.com>
2021-05-28 00:23:07 +0300

    task3 v1.0

--- 3_dict_to_csv.py
+++ 3_dict_to_csv.py
@@ -10,12 +10,37 @@
 
 """
 
+import csv, random
+
+jobs = ['токарь', 'пекарь', 'аптекарь', 'lawer', 'jobless']
+my_list = []
+
+def names_gen(l):
+    f = ['бек', 'джан', 'ли', 'лей', 'слав']
+    n = ['ив', 'ол', 'ал', 'ни', 'вла', 'эр', 'джэт']
+    name = ''
+    for c in range(l-(random.randrange(1))):
+        name += random.choice(n)
+
+    name += random.choice(f)
+    return name
+
+
+for e in range(100):
+    entry = {'name':names_gen(2), 'age': random.randrange(15,63), 'job': random.choice(jobs)}
+    my_list.append(entry)
+
+
+
 def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+    with open('test.csv', 'w', encoding='utf-8', newline='') as cf:
+        fields = ['name', 'age', 'job']
+
+        writer = csv.DictWriter(cf, fields, delimiter=';')
+        writer.writeheader()
+        for y in my_list:
+            writer.writerow(y)
+
 
 if __name__ == "__main__":
     main()
