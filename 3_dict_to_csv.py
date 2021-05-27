5bf1f42a3da65493832859b16b28c7ffa5a2b087
Evgeny Surogin <sacccp@gmail.com>
2021-05-27 16:20:38 +0300

    homework2

--- 3_dict_to_csv.py
+++ 3_dict_to_csv.py
@@ -9,13 +9,27 @@
 2. Запишите содержимое списка словарей в файл в формате csv
 
 """
+import csv
+
 
 def main():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    staff = [
+        {'name': 'Маша', 'age': 25, 'job': 'Scientist'},
+        {'name': 'Вася', 'age': 8, 'job': 'Programmer'},
+        {'name': 'Эдуард', 'age': 48, 'job': 'Big boss'},
+        {'name': 'Федя', 'age': 33, 'job': 'Cleaner'}
+    ]
+    with open('my_staff.csv', 'w', newline='', encoding='utf-8') as my_staff:
+        fields = ['name', 'age', 'job']
+        writer = csv.DictWriter(my_staff, fields, delimiter=';')
+        writer.writeheader()
+        for member in staff:
+            writer.writerow(member)
+
 
 if __name__ == "__main__":
     main()
