9e1cde50de6316296685b35258e8e8baed65f486
Daria Ivanchenko <dariaivanchenko@yandex.ru>
2021-05-29 18:34:36 +0300

    Homework of the second week

--- 1_date_and_time.py
+++ 1_date_and_time.py
@@ -9,19 +9,29 @@
 """
 
 def print_days():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+    from datetime import datetime, timedelta
 
+    dt_today = datetime.now()
+    dt_today.strftime('%d.%m.%Y %H:%M')
+    print(dt_today)
 
+    delta1 = timedelta(days=1)
+    dt_yesterday = dt_today - delta1
+    dt_yesterday.strftime('%d.%m.%Y %H:%M')
+    print(dt_yesterday)
+
+    delta2 = timedelta(days=30)
+    dt_month_ago = dt_today - delta2
+    dt_month_ago.strftime('%d.%m.%Y %H:%M')
+    print(dt_month_ago)
+
+# Превратите строку "01/01/20 12:10:03.234567" в объект datetime
 def str_2_datetime(date_string):
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+    from datetime import datetime
+
+    # date_string = '01/01/20 12:10:03.234567'
+    date_dt = datetime.strptime(date_string, '%m/%d/%y %H:%M:%S.%f')
+    print(date_dt)
 
 if __name__ == "__main__":
     print_days()
