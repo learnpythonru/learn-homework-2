d9baa3c86e4a6261e417991fdfd04b7f0a17f52c
gr4mka <88055914+gr4mka@users.noreply.github.com>
2021-12-06 22:56:05 +0300

    task#2

--- 2_files.py
+++ 2_files.py
@@ -4,8 +4,8 @@
 Работа с файлами
 
 
-1. Скачайте файл по ссылке https://www.dropbox.com/s/sipsmqpw1gwzd37/referat.txt?dl=0
-2. Прочитайте содержимое файла в перменную, подсчитайте длинну получившейся строки
+1. Скачайте файл по ссылке https://www.dropbox.com/s/sipsmqpw1gwzd37/referat.txt?dl=0 
+2. Прочитайте содержимое файла в перменную, подсчитайте длинну получившейся строки 
 3. Подсчитайте количество слов в тексте
 4. Замените точки в тексте на восклицательные знаки
 5. Сохраните результат в файл referat2.txt
@@ -13,18 +13,27 @@
 
 #import urllib.request
 # urllib.request.urlretrieve('https://www.dropbox.com/s/sipsmqpw1gwzd37/referat.txt?dl=0', 'korneev.txt') # это было слишком просто, чтобы быть правдой. ссылка не прямая, скачался код всей страницы %)
-def main():
+
+def filesize(): 
     with open('referat.txt', 'r', encoding='utf-8') as f:
-    
-        for line in f:
-            line = line.replace('\n', '')
-            print(len(line.strip())) #кол-во символов по абзацам
-            line = line.replace('.', '!')
-            print(line)
+        print('Общее количество символов в файле (длина строки):' + str(len(f.read())))
+        f.close() # этим надо заканчивать функцию работы с файлами?
 
+def counter():
+    with open('referat.txt', 'r', encoding='utf-8') as f:
+        data = f.read()
+        words = data.split() # метод для возвращения списка слов в строке
+        print('Общее количество слов в тексте:', len(words)) 
+        f.close()
 
-
-
+def spaces_switch():
+    with open("referat.txt", 'r', encoding='utf-8') as fr: # начальный файл для чтения
+        with open("referat2.txt", 'w', encoding='utf-8') as fw: # т.к. данного файла нет -> создается для записи
+                for line in fr.readlines(): # построчное чтение
+                    line = line.replace('.', '!') # замена точек на восклицательный знак
+                    fw.writelines(line) # запись во второй файл измененные строки первого
 
 if __name__ == "__main__":
-    main()
+    filesize()
+    counter()
+    spaces_switch()
