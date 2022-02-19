f6c4b165d794f6ceff8b002804a573fd678cdf2c
astra <zorkin.roman@gmail.com>
2022-02-19 18:45:20 +0300

    Solved first exercise

--- 1_date_and_time.py
+++ 1_date_and_time.py
@@ -7,22 +7,36 @@
 2. Превратите строку "01/01/20 12:10:03.234567" в объект datetime
 
 """
-
-def print_days():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+from datetime import datetime, timedelta
 
 
-def str_2_datetime(date_string):
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+def print_days() -> None:
+    """The function outputs the specified dates to the console.
 
-if __name__ == "__main__":
+    The function outputs to the console: today, yesterday, 30 days ago.
+    """
+    number_of_days_ago = 30
+    today: datetime = datetime.now()
+    delta_days: timedelta = timedelta(days=1)
+    dates: list[datetime] = [today]
+    dates.append(today - delta_days)
+    dates.append(today - delta_days * number_of_days_ago)
+    for day in dates:
+        print(day.strftime('%d-%m-%y'))
+
+
+def str_2_datetime(date_string: str) -> datetime:
+    """The function convert date from string to datetime object.
+
+    Args:
+        date_string (str): date as strihg.
+
+    Returns:
+        datetime: set date as datetime object.
+    """
+    return datetime.strptime(date_string, '%d/%m/%y %H:%M:%S.%f')
+
+
+if __name__ == '__main__':
     print_days()
-    print(str_2_datetime("01/01/20 12:10:03.234567"))
+    print(str_2_datetime('01/01/20 12:10:03.234567'))
