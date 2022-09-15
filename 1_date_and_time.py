c25042708b9b56de6e59d08fc0f1b9dafb727da9
Semenov Artem <semenov.artem@hotmail.com>
2022-09-16 00:13:46 +0300

    Второе домашнее задание

--- 1_date_and_time.py
+++ 1_date_and_time.py
@@ -7,13 +7,18 @@
 2. Превратите строку "01/01/20 12:10:03.234567" в объект datetime
 
 """
+from datetime import date, datetime, timedelta
+
 
 def print_days():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    dt_now = date.today()
+    dt_yesterday = dt_now - timedelta(days=1)
+    dt_30 = dt_now - timedelta(days=30)
+    print(f'Сегодня {dt_now.strftime("%d.%m.%Y")}, вчера было {dt_yesterday.strftime("%d.%m.%Y")}, 30 дней назад {dt_30.strftime("%d.%m.%Y")}')
 
 
 def str_2_datetime(date_string):
@@ -21,7 +26,8 @@ def str_2_datetime(date_string):
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    dt = datetime.strptime(date_string,"%m/%d/%y %H:%M:%S.%f")
+    return dt
 
 if __name__ == "__main__":
     print_days()
--- 3_dict_to_csv.py
+++ 3_dict_to_csv.py
@@ -9,13 +9,27 @@
 2. Запишите содержимое списка словарей в файл в формате csv
 
 """
+import csv
 
 def main():
+    characters = [
+        {'name':'Mabel Pines', 'age':'12', 'job': "Dipper's sister"},
+        {'name':'Dipper Pines', 'age':'12', 'job': "Mabel's sister"},
+        {'name':'Stan Pines', 'age':'70', 'job': "The greedy, grumpy, yet loving great-uncle of Dipper and Mabel Pines"},
+        {'name':'Soos Ramirez', 'age':'22', 'job': "Handyman at the Mystery Shack"},
+        {'name':'Soos Ramirez', 'age':'15', 'job': "Part-time employee at the Mystery Shack"},
+
+    ]
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    with open('characters.csv', 'w', encoding='utf-8', newline='') as f:
+        fields = ['name', 'age', 'job']
+        wr = csv.DictWriter(f, fields, delimiter=';')
+        wr.writeheader()
+        for user in characters:
+            wr.writerow(user)
 
 if __name__ == "__main__":
     main()
