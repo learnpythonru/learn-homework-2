2d8637e9a9a803c29dd83a04a51e2f5e01f91e01
Prokhorov Mark <mxlzubenko@yangex.ru>
2023-02-18 22:54:20 +0300

    done

--- 1_date_and_time.py
+++ 1_date_and_time.py
@@ -7,21 +7,21 @@
 2. Превратите строку "01/01/20 12:10:03.234567" в объект datetime
 
 """
+from datetime import *
+
 
 def print_days():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+    date = datetime.now()
+    delta_1 = timedelta(days=1)
+    delta_30 = timedelta(days=30)
+    date_1 = date - delta_1
+    date_30 = date - delta_30
+    return date.strftime('%d.%m.%Y'), date_1.strftime('%d.%m.%Y'), date_30.strftime('%d.%m.%Y')
 
 
 def str_2_datetime(date_string):
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+    date = datetime.strptime(date_string, '%m/%d/%y %H:%M:%S.%f')
+    return date
 
 if __name__ == "__main__":
     print_days()
--- 2_files.py
+++ 2_files.py
@@ -12,11 +12,18 @@
 """
 
 def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+    with open('referat.txt', 'r', encoding='utf8') as f:
+        str = f.read()
+        len_str = len(str)
+        words = 0
+        for word in str.split():
+            words += 1
+
+    with open('referat2.txt', 'w', encoding='utf8') as text:
+        point_to_exclamation_point = str.replace('.', '!')
+        text.write(point_to_exclamation_point)
+
+    return len_str, words
 
 if __name__ == "__main__":
     main()
