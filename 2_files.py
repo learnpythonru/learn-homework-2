9e375bc4b1b0188f5d1cd4e0c574458095aa066f
Sergey Ilin <ilin_sergey90@mail.ru>
2023-02-19 16:51:09 +0300

    files complite

--- 2_files.py
+++ 2_files.py
@@ -11,12 +11,17 @@
 5. Сохраните результат в файл referat2.txt
 """
 
+
 def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+    with open("referat.txt", "r", encoding="utf-8") as file:
+        content = file.read()
+    lenght = len(content)
+    count_of_word = len(content.split(' '))
+    new_str = content.replace(".", "!")
+    with open("referat2.txt", "w", encoding="utf-8") as file:
+        file.write(new_str)
+    print(f"Длина строки: {lenght} символов, количество слов: {count_of_word}")
+    
 
 if __name__ == "__main__":
     main()
