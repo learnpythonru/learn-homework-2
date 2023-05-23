4192da708d3f5e25b5123299b2ed685770cc965f
MSFirsov <mxfrsv@gmail.com>
2023-05-23 20:59:08 +0300

    MSFirsov_homework_2

--- 1_date_and_time.py
+++ 1_date_and_time.py
@@ -7,21 +7,17 @@
 2. Превратите строку "01/01/20 12:10:03.234567" в объект datetime
 
 """
-
+from datetime import datetime, date, timedelta
 def print_days():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+
+    print('yesterday:', date.today() - timedelta(days=1))
+    print('today:', date.today())
+    print('30 day ago:', date.today() - timedelta(days=30))
 
 
 def str_2_datetime(date_string):
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+
+    return datetime.strptime(date_string, '%m/%d/%y %H:%M:%S.%f')
 
 if __name__ == "__main__":
     print_days()
