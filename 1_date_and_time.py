9fd4d2af6df0c71167d71ff389aa1d44f356e868
Alexey <fav1290@gmail.com>
2023-05-29 16:39:36 +0300

    Cosmetic changes

--- 1_date_and_time.py
+++ 1_date_and_time.py
@@ -3,12 +3,12 @@ import datetime
 
 # Напечатайте в консоль даты: вчера, сегодня, 30 дней назад
 def print_days():
-    nowadate = datetime.date.today()
-    yesterdelta = datetime.timedelta(days=1)
+    current_date = datetime.date.today()
+    daily_delta = datetime.timedelta(days=1)
     monthly_delta = datetime.timedelta(days=30)
-    print(nowadate.strftime('%d/%m/%Y'))
-    print((nowadate - yesterdelta).strftime('%d/%m/%Y'))
-    print((nowadate - monthly_delta).strftime('%d/%m/%Y'))
+    print(current_date.strftime('%d/%m/%Y'))
+    print((current_date - daily_delta).strftime('%d/%m/%Y'))
+    print((current_date - monthly_delta).strftime('%d/%m/%Y'))
 
 
 # Превратите строку "01/01/20 12:10:03.234567" в объект datetime
